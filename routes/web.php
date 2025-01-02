ec754db5584a92303ef305f6ec52127fb8d272f8
mojo <56223696+burnjoe@users.noreply.github.com>
2025-01-02 21:40:31 +0800

    explored various ways to interact with database

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,34 @@
 <?php
 
+use App\Models\Student;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+/*
+ * We can interact with the database either through:
+ * 1. Using raw SQL queries
+ * 2. Query builder
+ * 3. Eloquent ORM
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    // 1. Raw SQL Queries
+    $users = DB::select('select * from users');
+    // dd($users);  // die dump
+
+    // 2. Query Builder
+    $users = DB::table('users')->select(['name', 'email'])
+        ->whereNotNull('email')
+        ->orderBy('name')
+        ->get();
+    // dd($users);
+
+    // 3. Eloquent ORM
+    $students = Student::all();
+    // dd($students);
+    foreach ($students as $student) {
+        echo $student->name . "<br>";
+    }
 });
 
 // Handles the /hello request
